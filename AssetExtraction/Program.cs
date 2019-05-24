9c93951a7fa8caaccbce043b454b5737587165bf
Martin VN <m4rtini89@gmail.com>
2019-05-24 17:33:30 +0200

    Started a simple AssetExtraction project

---
+++ AssetExtraction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using UELib;
+using UELib.Core;
+
+namespace AssetExtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 2)
+            {
+                Console.WriteLine("Usage: -p \"Path to package\"");
+            }
+            var pathToPackage = args[1];
+            var package = UnrealLoader.LoadFullPackage(pathToPackage, System.IO.FileAccess.Read);
+
+            var staticMeshActors = new List<UObject>();
+
+            foreach (UObject obj in package.Objects)
+            {
+                if (obj.IsClassType("StaticMeshActor") )
+                {
+                    staticMeshActors.Add(obj);
+                }
+            }
+
+            foreach (var actor in staticMeshActors)
+            {
+                Console.WriteLine(actor.Decompile());
+            }
+        }
+    }
+}
